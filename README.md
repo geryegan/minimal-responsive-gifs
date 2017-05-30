3dbeb6ac6a4347c08f809100f1dcbfce00ec0fda
Gery Egan <gery.egan@gmail.com>
2017-05-30 10:00:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Minimal Responsive GIF Search Gallery
+
+Simple minimal responsive GIF search gallery using 
+
+## To get started
+```sh
+# Install dependencies
+npm install
+#Run App
+npm start
---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
+import axios from 'axios';
+import TextField from 'material-ui/TextField'
+import FontIcon from 'material-ui/FontIcon';
+import FlatButton from 'material-ui/FlatButton'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { cyan400 } from 'material-ui/styles/colors';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchValue: '',
+      gifs: []
+    };
+  }
+  //handle search text input changes
+  handleChange(event) {
+    this.setState({ searchValue: event.target.value });
+  }
+
+  //search for gifs using giphy api
+  submit(){
+    axios.get('http://api.giphy.com/v1/gifs/search?q=' + this.state.searchValue + '&api_key=dc6zaTOxFJmzC')
+      .then(
+        (data) => {
+          this.setState({
+            gifs: data.data.data
+          })
+        }
+      )
+  }
+  // remove gifs
+  clear(){
+    this.setState({ gifs: [], searchValue: '' })
+  }
+
+  //map over gifs in state and render
+  gifRender(){
+    return this.state.gifs.map(
+      (gif) => <img key={gif.id} src={gif.images.fixed_height.url} alt='a gif' />
+    )
+  }
+
+  //handle submission via Enter key
+  enterKeyDownHandler(e) {
+    if(e.key === 'Enter'){
+      this.submit();
+    }
+  }
+
+  //return different styling based off of whether or not there are gifs being viewed
+  inputFieldStyle() {
+    if (this.state.gifs.length > 0) {
+      return styles.inputStyleGifs;
+    } else return styles.inputStyleEmpty;
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="App">
+          <div style={this.inputFieldStyle()}>
+            <TextField
+              inputStyle={{ color: cyan400 }}
+              underlineFocusStyle={{ borderColor: cyan400 }}
+              hintStyle={{ color: '#FFF'}}
+              hintText='Search for a gif!'
+              onKeyDown={this.enterKeyDownHandler.bind(this)}
+              value={this.state.searchValue}
+              onChange={this.handleChange.bind(this)}
+              type='text'
+            />
+            <div style={styles.buttonContainer}>
+              <FlatButton onClick={this.submit.bind(this)}>
+                <FontIcon 
+                className="material-icons" 
+                color={cyan400} 
+                style={styles.iconStyle}>
+                  search
+                </FontIcon>
+              </FlatButton>
+              <FlatButton onClick={this.clear.bind(this)}>
+                <FontIcon 
+                className="material-icons" 
+                color={cyan400} 
+                style={styles.iconStyle}>
+                  restore
+                </FontIcon>
+              </FlatButton>
+            </div>
+          </div>
+
+          <div className='images'>
+            <CSSTransitionGroup
+              transitionName='fade'
+              component='div'
+              transitionEnterTimeout='750'
+              transitionLeaveTimeout='750'
+            >
+              {this.gifRender()}
+            </CSSTransitionGroup>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  iconStyle: {
+    marginTop: 6
+  },
+  buttonContainer: {
+    display: 'inline-block'
+  },
+  inputStyleEmpty: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%', 
+    marginLeft: '-150px',
+    marginTop: '-150px'
+  },
+  inputStyleGifs: {
+    margin: '50px'
+  }
+}
+
+export default App;
